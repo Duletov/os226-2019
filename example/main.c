64520d07fd0121bf8f4fa32d7e8fe0126d23cac1
Duletov <duletov.dmitriy@gmail.com>
2019-09-09 02:00:24 +0300

    Another try

--- example/main.c
+++ example/main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
 		int i, max, ans;
 		max = 0;
 		
-		scanf("%c", &data);
+		data = getchar();
 				
 		while(data != EOF){
 			data = data - '0';
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
 					ans = data;
 				}
 			}
-			scanf("%c", &data);
+			data = getchar();
 		}
 		
 		printf("%d\n", ans);
